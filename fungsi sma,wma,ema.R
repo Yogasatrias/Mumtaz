7cb648b4e7299f5676660e2dd673db70be7cc1db
Yogasatrias <37947121+Yogasatrias@users.noreply.github.com>
2018-06-02 09:20:23 +0700

    Add files via upload

---
+++ fungsi sma,wma,ema.R
@@ -0,0 +1,37 @@
+x=scan ()
+
+an.tek<-function(num, x, n){
+  nx<-length(x)
+  y<-numeric(nx)
+  
+switch(num, 
+       satu = {
+         for(i in n:nx) {
+       y[i] = mean(x[(i - n + 1):i])}
+       return(data.frame(cbind(x,SMA=y)))  
+         }, 
+         dua = {
+           SMA<-numeric(nx)
+           for (i in n:n) {
+             SMA[i]=mean(x[(i- n + 1):i])}
+         a=2/(n+1)
+         y[n-1]=SMA[n]
+           for(i in n:nx){
+             y[i]=a*(x[i]-y[i-1])+y[i-1]}
+         return(data.frame(cbind(x, EMA=y)))
+          },
+         tiga = {
+           for(i in n:nx){
+             y[i] = 2*sum((n:1)*x[i:(i- n+1)])/(n*(n+1))}
+return(data.frame(cbind(x, WMA=y)))
+         }
+)
+       }
+an.tek(1,x,10)
+
+an.tek("dua",x,10)
+an.tek("tiga",x,10)
+
+
+
+
